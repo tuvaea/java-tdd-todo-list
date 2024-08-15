434f63b7121e2c97c67df5234a2ffbac0b1c2edc
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-08-15 10:30:22 +0200

    Created test and dummy function for getting tasks.

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,13 +1,21 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
 
     public class Task{
 
     }
 
+    ArrayList<Task> todoList = new ArrayList<>();
+
     public Boolean addTask(String taskName){
         return true;
+    }
+
+    public String getTasks(ArrayList<Task> todoList){
+        return "Todo list is empty";
     }
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,8 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class TodoListTest {
     @Test
     public void testAddTask() {
@@ -10,4 +12,23 @@ class TodoListTest {
 
         Assertions.assertTrue(todoList.addTask("Test task 1"));
     }
+
+    @Test
+    public void testGetTasks(){
+        TodoList todoList = new TodoList();
+        // Test if todo list is empty
+        Assertions.assertEquals("Todo list is empty", todoList.getTasks(todoList.todoList));
+
+        todoList.addTask("Test task 1");
+        todoList.addTask("Test task 2");
+        todoList.addTask("Test task 3");
+
+        ArrayList<String> test = new ArrayList<>();
+        test.add("Test task 1");
+        test.add("Test task 2");
+        test.add("Test task 3");
+        // Test if todo list is not empty
+        Assertions.assertEquals(test.toString(), todoList.getTasks(todoList.todoList));
+    }
+
 }
